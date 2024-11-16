18dbcb33ee5c0afe0331bfe1426fba6cac9aa317
TranTienDat03 <trantiendat2003qb@gmail.com>
2024-11-16 22:29:08 +0700

    xinchao

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+import './App.css'
+
+// let guest = 0;
+
+// function Cup(){
+  
+//   guest = guest + 1;
+//   return <h2> Tea tran tien dat uoong nuoc #{guest}</h2>;
+// }
+// export default function TesSet(){
+//   return(
+//     <>
+//     <Cup />
+//     <Cup />
+//     <Cup />
+//     </>
+//   )
+// }
+
